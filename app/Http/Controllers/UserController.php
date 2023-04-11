3874d7257e06f25c6c9a8024a0e20f67664b4ce9
Wanderson Silva <wandersonsilva768@gmail.com>
2023-04-11 01:33:47 -0300

    feat: adição das rotas de teste da aplicação

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index ()
+    {
+        return view('users.index');
+    }
+
+    public function show ($id)
+    {
+        return view('users.show', [
+            'id' => $id
+        ]);
+    }
+}
---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,1 @@
+usuário único $id
